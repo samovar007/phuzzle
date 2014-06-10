e0eddbf1acf26e3c987ae7ce2129882af07ecd0d
Vasily Samoylov <sam_vm@mail.ru>
2014-06-10 09:49:39 +0400

    убрал счетчик с твиди, а на промежуточную страницу поставил

--- www/toSn/index.php
+++ www/toSn/index.php
@@ -30,6 +30,14 @@ $thisUrl = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 <meta property="og:url" content="<?= addslashes($thisUrl)?>">
 <meta property="og:description" content="">
 <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51202174-1', 'phuzzle.ru');
+  ga('send', 'pageview');
+	
 	location.href='<?=addslashes($url)?>';
 </script>	
 </head><body>
