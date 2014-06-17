9ed30414baf81a75452615659a568a65f19dc5cd
Vasily Samoylov <sam_vm@mail.ru>
2014-06-17 15:57:21 +0400

    пятнашки

--- www/toSn/index.php
+++ www/toSn/index.php
@@ -14,7 +14,7 @@ $thisUrl = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 <title>Фазлы - фотопазлы</title>
 
 <meta name="description" content="Пазл из любого изображения. Делитесь ссылками на свои пазлы в соцсетях. Подходит как взрослым, так и детям. Есть минутка - фазлы!">
-<meta name="keywords" content="Пазлы, развлечения, фото, фотопазлы, фазлы">
+<meta name="keywords" content="Пазлы, развлечения, фото, фотопазлы, фазлы, пятнашки, фрубик">
 <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
