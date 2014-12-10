0a8c7a09074cebde81fb16ea4a3c812c8668906a
Vasily Samoylov <sam_vm@mail.ru>
2014-12-10 10:33:58 +0300

    коммент к файлу

--- www/js/findPair.js
+++ www/js/findPair.js
@@ -1,6 +1,7 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Найди пару
+ * @author: Samoylov Vasily
+ * devphuzzle@gmail.com
  */
 var findPair = (function(){
 	
